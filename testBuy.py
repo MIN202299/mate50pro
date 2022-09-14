1e69e511990a69e14c7a39edec378a1c0cd9e222
段春旺 <203852295@qq.com>
2022-09-14 19:44:24 +0800

    testBuy

--- testBuy.py
+++ testBuy.py
@@ -1,4 +1,5 @@
 from ast import main
+from sqlite3 import Timestamp
 from selenium import webdriver
 from multiprocessing import Process
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -13,6 +14,9 @@ GOODS_URL = 'https://www.vmall.com/product/10086238622707.html'
 # chrome_driver path mac os
 CHROME_DRIVER_PATH = os.path.join(os.path.abspath('.'), 'webdriver/chromedriver')
 
+BEGIN_GO = '2022-09-15 10:07:50'
+END = '2022-09-15 10:10:00'
+
 # 开启的drivers
 drs = []
 
@@ -23,29 +27,36 @@ def buyPhone():
   driver.get(GOODS_URL)
   # 等待元素加载完全
   driver.implicitly_wait(10)
-  # 在此期间收动登录
+  # begin time
+  Timestamp = time.mktime(time.strptime(BEGIN_GO, '%Y-%m-%d %H:%M:%S'))
+  EndTimestamp = time.mktime(time.strptime(END, '%Y-%m-%d %H:%M:%S'))
   mall_btn = driver.find_element(By.ID, 'pro-operation').find_element(By.CLASS_NAME, 'product-button02')
   if mall_btn.text == '立即登录':
     mall_btn.click()
+  print('-----------请在120秒内手动扫码登录账号准备开抢---------------------')  
   # 在此期间手动登录  
-  time.sleep(60)
+  time.sleep(120)
+  print('[{0}] {1}:准备开抢'.format(os.getpid(), time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
   while True:
-    try:
-        mall_btn = driver.find_element(By.ID, 'pro-operation').find_element(By.CLASS_NAME, 'product-button02')
-        print(mall_btn.get_attribute('class'))
-        if mall_btn.get_attribute('class') != 'product-button02 disabled':
-            mall_btn.click()
-            print('click')
-    except Exception as err:
-        mall_btn = driver.find_element(By.ID, 'pro-operation').find_element(By.CLASS_NAME, 'product-button02')
-        print(err)
+    if time.time() > Timestamp:
+        try:
+          mall_btn = driver.find_element(By.ID, 'pro-operation').find_element(By.CLASS_NAME, 'product-button02')
+          print(mall_btn.get_attribute('class'))
+          if mall_btn.get_attribute('class') != 'product-button02 disabled':
+              mall_btn.click()
+              print('click')
+        except Exception as err:
+            mall_btn = driver.find_element(By.ID, 'pro-operation').find_element(By.CLASS_NAME, 'product-button02')
+            print(err)
+    if time.time() > EndTimestamp:
+        break
 
 
 if __name__=='__main__':
   process_list = []
   cup_nums = os.cpu_count()
   # 正式切换为cup_nums
-  for i in range(4):
+  for i in range(cup_nums):
     p = Process(target=buyPhone)
     process_list.append(p)
   for p in process_list:
